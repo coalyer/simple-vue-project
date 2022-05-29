c27b91e721db25a6b2bbcaa4e420aedd7a49b3a0
coalyer <liyang_qifengle@126.com>
2022-05-30 00:32:47 +0800

    feat:使用sass css预处理器

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -34,5 +34,36 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.login {
+  width: 100%;
+  height: 100%;
+  background: #000;
+  .l-form {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translateY(-50%) translateX(-50%);
+    width: 300px;
+    margin: auto;
+    border-radius: 8px;
+    background: #fff;
+    padding: 20px;
+    .l-tip {
+      text-align: center;
+      font-size: 24px;
+      font-weight: bold;
+    }
+    .el-form {
+      width: 100%;
+      margin: auto;
+      margin-top: 20px;
+      .el-form-item {
+        button {
+          width: 100%;
+        }
+      }
+    }
+  }
+}
 </style>
