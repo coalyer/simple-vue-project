1ae0bd77baeb39b6f5cd871ba7923f05c442feb7
coalyer <liyang_qifengle@126.com>
2022-05-30 00:48:27 +0800

    feat:安装normalize.css

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入路由
 import router from './router'
 // 引入element-ui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 引入normalize.css
+import 'normalize.css'
 
 Vue.use(ElementUI)
 
