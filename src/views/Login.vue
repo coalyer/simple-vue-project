f6db1b9f68ff257d591de583b10763c7deeab181
coalyer <liyang_qifengle@126.com>
2022-05-29 23:58:23 +0800

    feat:使用elementUI组件库

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,36 @@
 <template>
-  <div>这里是login页面</div>
+  <div class="login">
+    <div class="l-form">
+      <div class="l-tip">图书馆管理系统</div>
+      <el-form ref="form" :model="form">
+        <el-form-item>
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="form.password" type="password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data () {
+    return {
+      form: {
+        name: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () { }
+  }
 }
 </script>
 
