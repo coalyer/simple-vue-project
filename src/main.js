2235bb987db930595f3b33460ad1063ee4c8d7ef
coalyer <liyang_qifengle@126.com>
2022-05-30 07:46:40 +0800

    feat:使用vee-validate表单验证插件

--- src/main.js
+++ src/main.js
@@ -10,6 +10,13 @@ import 'normalize.css'
 // 引入mock
 import './mock.js'
 
+// 引入表单验证
+import VeeValidate, { Validator } from 'vee-validate'
+Vue.use(VeeValidate, { fieldsBagName: 'vee-fields' })
+// 汉化表单验证
+import zhCN from 'vee-validate/dist/locale/zh_CN'
+Validator.localize('zh_CN', zhCN)
+
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,10 +4,31 @@
       <div class="l-tip">图书馆管理系统</div>
       <el-form ref="form" :model="form">
         <el-form-item>
-          <el-input v-model="form.name"></el-input>
+          <el-input
+            v-model="form.name"
+            v-validate="{required:true}"
+            name="name"
+            :class="{'is-danger':errors.has('name')}"
+            data-vv-as="用户名"
+            placeholder="请输入用户名"
+          ></el-input>
         </el-form-item>
         <el-form-item>
-          <el-input v-model="form.password" type="password"></el-input>
+          <FormErrorMessage v-if="errors.has('name')">{{ errors.first('name') }}</FormErrorMessage>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="form.password"
+            type="password"
+            v-validate="{required:true}"
+            name="password"
+            :class="{'is-danger':errors.has('password')}"
+            data-vv-as="密码"
+            placeholder="请输入密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <FormErrorMessage v-if="errors.has('password')">{{ errors.first('password') }}</FormErrorMessage>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="onSubmit">登录</el-button>
@@ -19,8 +40,12 @@
 
 <script>
 import login from '../api/login.js'
+import FormErrorMessage from '../components/common/FormErrorMessage.vue'
 export default {
   name: 'Login',
+  components: {
+    FormErrorMessage
+  },
   data () {
     return {
       form: {
@@ -31,25 +56,29 @@ export default {
   },
   methods: {
     onSubmit () {
-      login.submit(this.form).then(res => {
-        // console.log('res:', res);
-        if (res.data.status === 1) {
-          // 如果登录成功则跳转我index页面
-          this.$router.push('/index')
-        } else {
-          // 使用element-ui的message组件，显示登录报错信息
-          this.$message({
-            message: res.data.message,
-            type: 'error',
-            duration: 5000
+      this.$validator.validate().then(result => {
+        if (result) {
+          login.submit(this.form).then(res => {
+            // console.log('res:', res);
+            if (res.data.status === 1) {
+              // 如果登录成功则跳转我index页面
+              this.$router.push('/index')
+            } else {
+              // 使用element-ui的message组件，显示登录报错信息
+              this.$message({
+                message: res.data.message,
+                type: 'error',
+                duration: 5000
+              })
+            }
+          }).catch(error => {
+            this.$message({
+              message: error,
+              type: 'error',
+              duration: 5000
+            })
           })
         }
-      }).catch(error => {
-        this.$message({
-          message: error,
-          type: 'error',
-          duration: 5000
-        })
       })
     }
   }
@@ -87,5 +116,8 @@ export default {
       }
     }
   }
+}
+.is-danger input {
+  border-color: #ff3860;
 }
 </style>
