217f377adb24bbdb72373039c39b419b3a722a72
coalyer <liyang_qifengle@126.com>
2022-05-30 08:31:27 +0800

    feat:设置路由拦截

--- src/main.js
+++ src/main.js
@@ -12,6 +12,8 @@ import './mock.js'
 // 引入store
 import store from './store/index.js'
 
+import Cookies from 'js-cookie'
+
 // 引入表单验证
 import VeeValidate, { Validator } from 'vee-validate'
 Vue.use(VeeValidate, { fieldsBagName: 'vee-fields' })
@@ -22,6 +24,27 @@ Validator.localize('zh_CN', zhCN)
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
+
+// 设置路由拦截
+router.beforeEach((to, from, next) => {
+  let name = Cookies.get('name') || store.state.name
+  // 如果cookie没有过期或者store中有name值,则允许访问直接通过。否则就让用户登录
+  if (name) {
+    store.commit('loginIn', name)
+    next()
+  } else {
+    if (to.path == '/login') {
+      next()
+    } else {
+      next({
+        name: 'Login'
+      })
+      store.commit('loginOut')
+    }
+  }
+})
+
+router.afterEach(() => { })
 
 new Vue({
   router,
